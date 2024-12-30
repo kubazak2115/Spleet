ee2246188c54b4655193c60c8a4e7196f7489fa1
nbochenek <bochenek@student.agh.edu.pl>
2024-12-30 19:23:08 +0100

    tego typu GUI3

--- App/src/main/java/com/example/demo2/HelloController.java
+++ App/src/main/java/com/example/demo2/HelloController.java
@@ -1,46 +1,66 @@
 package com.example.demo2;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import model.Wydatek;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private TableView<Wydatek> tableView;
-
-    @FXML
-    private TableColumn<Wydatek, Integer> columnId;
+public class HelloController implements Initializable {
 
     @FXML
-    private TableColumn<Wydatek, String> columnData;
+    private TextField fieldName;
 
     @FXML
-    private TableColumn<Wydatek, String> columnKategoria;
+    private TextField fieldAmount;
 
     @FXML
-    private TableColumn<Wydatek, Double> columnWydatek;
-
-    private final ObservableList<Wydatek> wydatki = FXCollections.observableArrayList();
+    private DatePicker fieldDate;
 
     @FXML
-    public void initialize() {
-        // Powiązanie kolumn z właściwościami w modelu danych
+    private ChoiceBox<String> choiceCategory;
+
+    @FXML
+    private ChoiceBox<String> choiceType;
+
+    @FXML
+    private PieChart pieChart;
+
+    @FXML
+    private TableView<Object> tableView;
+
+    @FXML
+    private TableColumn<Object, String> columnId;
+
+    @FXML
+    private TableColumn<Object, String> columnData;
+
+    @FXML
+    private TableColumn<Object, String> columnKategoria;
+
+    @FXML
+    private TableColumn<Object, String> columnWydatek;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Dodanie opcji do ChoiceBox
+        choiceCategory.getItems().addAll("Kategoria 1", "Kategoria 2");
+        choiceType.getItems().addAll("Przychód", "Wydatek");
+
+        // Dodanie danych do PieChart
+        pieChart.setData(FXCollections.observableArrayList(
+                new PieChart.Data("Kategoria 1", 40),
+                new PieChart.Data("Kategoria 2", 60)
+        ));
+
+        // Ustawienie danych tabeli
         columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         columnData.setCellValueFactory(new PropertyValueFactory<>("data"));
         columnKategoria.setCellValueFactory(new PropertyValueFactory<>("kategoria"));
         columnWydatek.setCellValueFactory(new PropertyValueFactory<>("wydatek"));
-
-        // Dodanie przykładowych danych do tabeli
-        wydatki.add(new Wydatek(1, "2024-12-30", "Jedzenie", 150.50));
-        wydatki.add(new Wydatek(2, "2024-12-29", "Transport", 50.00));
-        wydatki.add(new Wydatek(3, "2024-12-28", "Rozrywka", 120.75));
-
-        // Przypisanie danych do TableView
-        tableView.setItems(wydatki);
     }
 }
